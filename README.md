2d7c0129c5887b6d1e3bf05c234c3e081a8c3f9d
Akib Zawayed <56231072+akib0079@users.noreply.github.com>
2021-04-20 16:02:43 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# EducationTemplate
---
+++ custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [767, 1],
+        pagination: true,
+        autoPlay: true
+    });
+});
